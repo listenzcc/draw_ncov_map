9fcc707400bc3dd65671ef835b46c781bd3ccf52
Chuncheng Zhang <listenzcc@mail.bnu.edu.cn>
2020-02-12 10:11:26 +0800

    Function add.
    Add mapper server.

--- local_profiles.py
+++ local_profiles.py
@@ -2,25 +2,43 @@
 Profiles of the project.
 """
 
+import os
 import logging
 
 REMOTE_URL = 'https://ncov.dxy.cn/ncovh5/view/pneumonia'
+BAIDU_AK = '62c28637e66010289bdef04310267902'
+
+inventory_dir = 'ncov_inventory'
+mapper_server_dir = 'maper_server_memory'
 
 
 class PROFILES():
     def __init__(self):
         # Basic profiles
-        self.inventory_dir = 'ncov_inventory'
+        self.inventory_dir = inventory_dir
+        self.mapper_server_dir = mapper_server_dir
         self.remote_url = REMOTE_URL
+        self.baidu_ak = BAIDU_AK
         # Invoke logging
         self._invoke_logging()
+        # Basic check
+        self._check_dirs()
 
     def _invoke_logging(self):
         logging.basicConfig(filename='log_ncov.log',
                             level=logging.DEBUG,
                             format='%(asctime)s - %(levelname)s - %(message)s')
 
+    def _check_dirs(self):
+        """
+        Check dirs if they exist, make them if not.
+        """
+        for dir in [self.inventory_dir,
+                    self.mapper_server_dir]:
+            if not os.path.exists(dir):
+                message = f'Dir not exists: {dir}. Make it.'
+                logging.warning(message)
+                os.mkdir(dir)
+
 
 profiles = PROFILES()
-
-
---
+++ mapper_server.py
@@ -0,0 +1,71 @@
+"""
+My custom mapper server.
+Get latitude and longitude using BAIDU API.
+"""
+
+import os
+import logging
+import geocoder
+import pandas as pd
+from local_profiles import profiles
+
+
+class MAPPER_SERVER():
+    def __init__(self):
+        logging.info('MAPPER_SERVER starts.')
+        self.memory_path = os.path.join(
+            profiles.mapper_server_dir, 'memory.json')
+        self._read_memory()
+
+    def _read_memory(self):
+        """
+        Read memory json file as DataFrame,
+        if file not exists, use empty DataFrame instead.
+        yield:
+            self.memory: the memory DataFrame
+        """
+        if os.path.exists(self.memory_path):
+            logging.info(f'Get memory from {self.memory_path}')
+            self.memory = pd.read_json(self.memory_path)
+        else:
+            logging.warning(
+                f'Memory file not exists. Use empty DataFrame instead.')
+            self.memory = pd.DataFrame(columns=['latitude',
+                                                'longitude'])
+
+    def solid_memory(self):
+        """
+        Write memory DataFrame into json file.
+        yield:
+            Write memory json file.
+        """
+        self.memory.to_json(open(self.memory_path, 'w'))
+
+    def checkout(self, name):
+        """
+        Check out position of name.
+        inputs:
+            name: name of location
+        outputs:
+            lat: latitude of location
+            lng: longitude of location
+        yield:
+            Update memory if not remembered
+        """
+        if name in self.memory.index:
+            lat = self.memory.loc[name].latitude
+            lng = self.memory.log[name].longtitude
+        else:
+            try:
+                g = geocoder.baidu(name, key=profiles.baidu_ak)
+                lat, lng = g.latlng
+                se = pd.Series(data={'latitude': lat,
+                                     'longitude': lng},
+                               name=name)
+                self.memory = self.memory.append(se)
+            except KeyError as err:
+                lat, lng = None, None
+                message = repr(err)
+                print(f'Error occured on checkout {name}: {message}')
+                logging.error(message)
+        return lat, lng
